96ec64af458af04e8f01b6e508e1b785642fbe12
Suleyman Meral <mrlslymn02@gmail.com>
2026-06-28 01:18:45 +0300

    Add OData controller with EDM model support
    Introduced oDataControllerController to enable OData queries in the API. The controller includes a static method to generate an EDM model with lower camel case configuration using ODataConventionModelBuilder.

---
+++ RentCarServer/src/RentCarServer.WebAPI/Controllers/oDataControllerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+namespace RentCarServer.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[EnableQuery]
+public class oDataControllerController : ControllerBase
+{
+   public static IEdmModel GetEdmModel()
+    {
+        ODataConventionModelBuilder builder = new();
+        builder.EnableLowerCamelCase();
+
+        return builder.GetEdmModel();
+    }
+}
