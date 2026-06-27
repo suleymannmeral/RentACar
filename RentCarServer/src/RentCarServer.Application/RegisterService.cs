1d91a1f8f942efec4958e6b17f724f8af7f270d3
Suleyman Meral <mrlslymn02@gmail.com>
2026-06-27 11:09:09 +0300

    First Commit Setup

---
+++ RentCarServer/src/RentCarServer.Application/RegisterService.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using RentCarServer.Application.Behaviours;
+using TS.MediatR;
+
+namespace RentCarServer.Application;
+
+public static  class RegisterService
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // Implementation for adding application services
+
+        services.AddMediatR(cfr =>
+        {
+            cfr.RegisterServicesFromAssembly(typeof(RegisterService).Assembly);
+            cfr.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            cfr.AddOpenBehavior(typeof(PermissionBehavior<,>));
+
+        });
+
+
+        services.AddValidatorsFromAssembly(typeof(RegisterService).Assembly);
+        return services;
+    }
+}
---
+++ RentCarServer/src/RentCarServer.Domain/Abstractions/Entity.cs
@@ -0,0 +1,43 @@
+﻿namespace RentCarServer.Domain.Abstractions;
+
+public abstract class Entity
+{
+
+    protected Entity()
+    {
+        Id = new IdentityId(Guid.CreateVersion7());  // crete sortable guid
+        Guid id = Id;
+        string idString = Id;
+        isActive = true;
+
+    }
+    public IdentityId Id { get; private set; }
+    public bool isActive { get; private set; }
+
+
+    public DateTimeOffset CretedAt { get;private set;  }
+    public IdentityId CreatedBy { get; private set; } = default!;
+    public DateTimeOffset? UpdatedAt { get; private set; }
+    public IdentityId? UpdatedBy { get; private set; } 
+    public bool IsDeleted { get; private set; } // soft delete flag
+    public DateTimeOffset? DeletedAt { get; private set; }
+    public IdentityId? DeletedBy { get; private set; }
+
+    public void SetStatus(bool isActive)
+    {
+        this.isActive = isActive;
+    }
+
+    public void Delete()
+    {
+        IsDeleted = true;
+     
+    }
+
+}
+
+public sealed record IdentityId(Guid value)
+{
+    public static implicit operator Guid(IdentityId id) => id.value;
+    public static implicit operator string(IdentityId id) => id.value.ToString();
+}
